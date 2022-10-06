10ede5155aeff1c82d83a6d28fb66cb8a50d2a89
anandamelati2022 <112836844+anandamelati2022@users.noreply.github.com>
2022-10-06 14:13:11 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,12 @@
+<?php
+//variable define by user
+$namaSiswa = 'Ananda melati';
+$umur = 20;
+$berat_badan = 55;
+//cetak
+//menggunakan concat
+echo 'Siswa bernama '.$namaSiswa.', umurnya '.$umur.
+' tahun dan berat badannya '.$berat_badan.' kg';
+//menggunakan doublenote tidak disarankan 
+echo "<hr/>Siswa bernama $namaSiswa , umurnya $umur.
+ tahun dan berat badannya $berat_badan kg";
---
+++ latihan2.php
@@ -0,0 +1,12 @@
+Dokumen Root PHP saya ada di <?= $_SERVER['DOCUMENT_ROOT'] ?>
+</br>File Latihan2.php berada di <?= $_SERVER['SCRIPT_FILENAME'] ?>
+<hr />
+<?php
+//variable konstanta
+define('LEMBAGA', 'NurulFikri Computer');
+define('PHI', 3.14);
+$jari2 = 15;
+$luas = PHI * $jari2 * $jari2;
+?>
+Saya sedang belajar pemrograman PHP di <?= LEMBAGA ?>
+<br />Luas lingkaran dengan jari-jari <?= $jari2 ?> cm = <?= $luas ?>
